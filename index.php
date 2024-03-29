f6489d364ca00c93b2ed9ebc97afb33ced85fc45
Ronald Borja <rborjaroman@gmail.com>
2024-03-29 13:27:39 -0500

    Ultimos arreglos index

--- index.php
+++ index.php
@@ -16,52 +16,52 @@
             <ul class="nav">
                     <li><a href="">CLIENTE NATURAL</a>
                         <ul class="clientes">
-                            <li><a href="../view/naturales/agregar.php">Añadir Cliente Natural</a>
-                            <li><a href="../view/naturales/buscar.php">Buscar Cliente Natural</a>
-                            <li><a href="../view/naturales/buscar.php">Actualizar Cliente Natural</a>
-                            <li><a href="../view/naturales/buscar.php">Eliminar Cliente Natural</a>
-                            <li><a href="../view/naturales/listar_todo.php">Listar Clientes Naturales</a>
+                            <li><a href="../backendbanco/view/naturales/agregar.php">Añadir Cliente Natural</a>
+                            <li><a href="../backendbanco/view/naturales/buscar.php">Buscar Cliente Natural</a>
+                            <li><a href="../backendbanco/view/naturales/buscar.php">Actualizar Cliente Natural</a>
+                            <li><a href="../backendbanco/view/naturales/buscar.php">Eliminar Cliente Natural</a>
+                            <li><a href="../backendbanco/view/naturales/listar_todo.php">Listar Clientes Naturales</a>
                         </ul>
                     </li>
 
                     <li><a href="">ORGANIZACIONES</a>
                         <ul class="organizacion">
-                            <li><a href="../view/organizacion/agregar.php">Añadir Organizacion</a>
-                            <li><a href="../view/organizacion/buscar.php">Buscar Organizacion </a>
-                            <li><a href="../view/organizacion/buscar.php">Actualizar Organizacion </a>
-                            <li><a href="../view/organizacion/buscar.php">Eliminar Organizacion </a>
-                            <li><a href="../view/organizacion/listar_todo.php">Listar Organizaciones</a>
+                            <li><a href="../backendbanco/view/organizacion/agregar.php">Añadir Organizacion</a>
+                            <li><a href="../backendbanco/view/organizacion/buscar.php">Buscar Organizacion </a>
+                            <li><a href="../backendbanco/view/organizacion/buscar.php">Actualizar Organizacion </a>
+                            <li><a href="../backendbanco/view/organizacion/buscar.php">Eliminar Organizacion </a>
+                            <li><a href="../backendbanco/view/organizacion/listar_todo.php">Listar Organizaciones</a>
                         </ul>
                     </li>
 
 
                     <li><a href="">CUENTAS</a>
                         <ul class="cuenta">
-                            <li><a href="../view/cuentas/agregar.php">Añadir Cuenta</a>
-                            <li><a href="../view/cuentas/buscar.php">Buscar Cuenta</a>
-                            <li><a href="../view/cuentas/buscar.php">Actualizar Cuenta</a>
-                            <li><a href="../view/cuentas/buscar.php">Eliminar Cuenta</a>
-                            <li><a href="../view/cuentas/listar_todo.php">Listar Cuentas</a>
+                            <li><a href="../backendbanco/view/cuentas/agregar.php">Añadir Cuenta</a>
+                            <li><a href="../backendbanco/view/cuentas/buscar.php">Buscar Cuenta</a>
+                            <li><a href="../backendbanco/view/cuentas/buscar.php">Actualizar Cuenta</a>
+                            <li><a href="../backendbanco/view/cuentas/buscar.php">Eliminar Cuenta</a>
+                            <li><a href="../backendbanco/view/cuentas/listar_todo.php">Listar Cuentas</a>
                         </ul>
                     </li>
 
                     <li><a href="">EMPLEADOS</a>
                         <ul class="empleado">
-                            <li><a href="../view/empleado/agregar.php">Añadir Empleado</a>
-                            <li><a href="../view/empleado/buscar.php">Buscar Empleado</a>
-                            <li><a href="../view/empleado/buscar.php">Actualizar Empleado</a>
-                            <li><a href="../view/empleado/buscar.php">Eliminar Empleado</a>
-                            <li><a href="../view/empleado/buscar.php">Listar Empleados</a>
+                            <li><a href="../backendbanco/view/empleado/agregar.php">Añadir Empleado</a>
+                            <li><a href="../backendbanco/view/empleado/buscar.php">Buscar Empleado</a>
+                            <li><a href="../backendbanco/view/empleado/buscar.php">Actualizar Empleado</a>
+                            <li><a href="../backendbanco/view/empleado/buscar.php">Eliminar Empleado</a>
+                            <li><a href="../backendbanco/view/empleado/buscar.php">Listar Empleados</a>
                         </ul>
                 
                     </li>
                     <li><a href="">SUCURSALES</a>
                     <ul class="sucursal">
-                            <li><a href="../view/sucursales/agregar.php">Añadir Sucursal</a>
-                            <li><a href="../view/sucursales/buscar.php">Buscar Sucursal</a>
-                            <li><a href="../view/sucursales/buscar.php">Actualizar Sucursal</a>
-                            <li><a href="../view/sucursales/buscar.php">Eliminar Sucursal</a>
-                            <li><a href="../view/sucursales/listar_todo.php">Listar Sucursales</a>
+                            <li><a href="../backendbanco/view/sucursales/agregar.php">Añadir Sucursal</a>
+                            <li><a href="../backendbanco/view/sucursales/buscar.php">Buscar Sucursal</a>
+                            <li><a href="../backendbanco/view/sucursales/buscar.php">Actualizar Sucursal</a>
+                            <li><a href="../backendbanco/view/sucursales/buscar.php">Eliminar Sucursal</a>
+                            <li><a href="../backendbanco/view/sucursales/listar_todo.php">Listar Sucursales</a>
                         </ul>
                     </li>
 
